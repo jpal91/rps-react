1814791237a3b148f4a7b08f835a96b67e5da425
jpal91 <98477057+jpal91@users.noreply.github.com>
2022-06-28 16:54:42 -0400

    got basic function working, trying alt approach

--- src/assets/base/typography.js
+++ src/assets/base/typography.js
@@ -1,18 +1,23 @@
+import colors from "./colors";
+
+const { text } = colors;
+
 const baseProperties = {
-    fontWeightBold: 700,
-    fontWeightRegular: 600,
-    fontFamily: "'Barlow Semi Condensed', sans-serif"
-}
+  fontWeightBold: 700,
+  fontWeightRegular: 600,
+  fontFamily: "'Barlow Semi Condensed', sans-serif"
+};
 
 const typography = {
-    h3: {
-        fontWeight: baseProperties.fontWeightBold,
-        fontFamily: baseProperties.fontFamily
-    },
-    h4: {
-        fontWeight: baseProperties.fontWeightRegular,
-        fontFamily: baseProperties.fontFamily
-    }
-}
+  h3: {
+    fontWeight: baseProperties.fontWeightBold,
+    fontFamily: baseProperties.fontFamily,
+    color: "white"
+  },
+  h4: {
+    fontWeight: baseProperties.fontWeightRegular,
+    fontFamily: baseProperties.fontFamily
+  }
+};
 
-export default typography
+export default typography;
--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -10,10 +10,26 @@ const ScoreBoard = () => {
           backgroundColor: "hsla(360, 100%, 100%, 0)",
           width: "100%",
           border: "2px solid",
+
           borderColor: "text.header",
           height: 150
         }}
-      ></Paper>
+      >
+        <Grid container item xs={12} sx={{ width: "100%", height: "100%" }}>
+          <Grid item xs={6} sx={{ flexDirection: "column", p: 2 }}>
+            <Typography variant="h3" sx={{ lineHeight: 0.75 }}>
+              ROCK
+            </Typography>
+            <Typography variant="h3" sx={{ lineHeight: 0.75 }}>
+              PAPER
+            </Typography>
+            <Typography variant="h3" sx={{ lineHeight: 0.75 }}>
+              SCISSORS
+            </Typography>
+          </Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
+      </Paper>
     </Grid>
   );
 };
