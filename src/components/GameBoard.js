7d96eb1b818b4f6139af0f891994902d4d57d3be
jpal91 <98477057+jpal91@users.noreply.github.com>
2022-06-28 17:28:10 -0400

    created new gameboard structure

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,31 +1,63 @@
 import Image from "material-ui-image";
 import Grid from "@mui/material/Grid";
 
-const GameBoard = () => {
+const GameBoard2 = () => {
   return (
-    <Grid container sx={{ width: 944 }}>
-      <Image
-        src="./images/icon-rock2.svg"
-        imageStyle={{ width: 200, height: 200 }}
-        style={{ position: "absolute", left: "45%", top: "489px", zIndex: 2 }}
-      />
-      <Image
-        src="./images/icon-paper2.svg"
-        imageStyle={{ width: 200, height: 200 }}
-        style={{ position: "absolute", left: "52%", top: "262px", zIndex: 2 }}
-      />
-      <Image
-        src="./images/icon-scissors2.svg"
-        imageStyle={{ width: 200, height: 200 }}
-        style={{ position: "absolute", left: "39%", top: "262px", zIndex: 2 }}
-      />
-      <Image
-        src="./images/bg-triangle.svg"
-        imageStyle={{ width: 250, height: 285 }}
-        style={{ position: "absolute", left: "44%", top: "350px" }}
-      />
+    <Grid container sx={{ width: 944, mt: 5 }}>
+      <Grid container item xs={12}>
+        <Grid
+          item
+          xs={6}
+          sx={{ justifyContent: "flex-end", p: 3, height: "auto" }}
+        >
+          <Image
+            src="./images/icon-scissors2.svg"
+            imageStyle={{ width: 200, height: 200, position: "relative" }}
+            style={{
+              zIndex: 1,
+              padding: 0,
+              height: 200,
+              backgroundColor: "hsla(360, 100%, 100%, 0)"
+            }}
+          />
+        </Grid>
+        <Grid item xs={6} sx={{ justifyContent: "flex-start", p: 3 }}>
+          <Image
+            src="./images/icon-paper2.svg"
+            imageStyle={{ width: 200, height: 200, position: "relative" }}
+            style={{
+              zIndex: 1,
+              padding: 0,
+              height: 200,
+              backgroundColor: "hsla(360, 100%, 100%, 0)"
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container item xs={12} sx={{ p: 1 }}>
+        <Image
+          src="./images/icon-rock2.svg"
+          imageStyle={{ width: 200, height: 200, position: "relative" }}
+          style={{
+            zIndex: 1,
+            padding: 0,
+            height: 200,
+            backgroundColor: "hsla(360, 100%, 100%, 0)"
+          }}
+        />
+        <Image
+          src="./images/bg-triangle.svg"
+          imageStyle={{ width: 250, height: 285 }}
+          style={{
+            zIndex: 0,
+            position: "absolute",
+            transform: "translateY(-7%)",
+            left: "43.75%"
+          }}
+        />
+      </Grid>
     </Grid>
   );
 };
 
-export default GameBoard;
+export default GameBoard2;
